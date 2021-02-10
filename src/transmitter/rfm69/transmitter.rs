8d69a039934640753d9ee1c3f7e0e5f4554a2509
Simon <16670268+dl1ne@users.noreply.github.com>
2021-02-10 11:54:26 +0100

    Update transmitter.rs
    Added remote configuration settings

--- src/transmitter/rfm69/transmitter.rs
+++ src/transmitter/rfm69/transmitter.rs
@@ -1,10 +1,12 @@
-use serial::{self, SerialPort};
+use serial::{self, SerialPort}; 
+use std::{thread, time};
+use std::io::Write;
 
-use crate::config::Config;
-use crate::transmitter::Transmitter;
+use config::Config;
+use transmitter::Transmitter;
 
 pub struct RFM69Transmitter {
-    serial: Box<dyn serial::SerialPort>
+    serial: Box<serial::SerialPort>
 }
 
 impl RFM69Transmitter {
@@ -25,13 +27,37 @@ impl RFM69Transmitter {
             })
             .expect("Unable to configure serial port");
 
+
+        
+        let cfg = [0x18 as u8];
+        let eot = [0x17 as u8];
+        let freq = config.rfm69.freq;
+        let power = [config.rfm69.output_level as u8];
+
+        let b1 = [ (freq >> 24) as u8 ];
+        let b2 = [ (freq >> 16) as u8 ];
+        let b3 = [ (freq >> 8) as u8 ];
+        let b4 = [ freq as u8 ];
+
+        let all = [cfg, b1, b2, b3, b4, cfg, power, eot];
+
+	for item in all.iter() {
+            for _ in 0..5 {
+   	        if !serial.write_all(item).is_err() {
+		    break;
+		}
+		thread::sleep(time::Duration::from_millis(10));
+	    }
+	}
+
         RFM69Transmitter { serial: Box::new(serial) }
     }
 }
 
 impl Transmitter for RFM69Transmitter {
-    fn send(&mut self, gen: &mut dyn Iterator<Item = u32>) {
+    fn send(&mut self, gen: &mut Iterator<Item = u32>) {
         for word in gen {
+
             let bytes = [
                 ((word & 0xff000000) >> 24) as u8,
                 ((word & 0x00ff0000) >> 16) as u8,
@@ -39,9 +65,11 @@ impl Transmitter for RFM69Transmitter {
                 (word & 0x000000ff) as u8,
             ];
 
-            if (*self.serial).write_all(&bytes).is_err() {
-                error!("Unable to write data to the serial port");
-                return;
+            for _ in 0..5 {
+               if !(*self.serial).write_all(&bytes).is_err() {
+                  break;
+               }
+               thread::sleep(time::Duration::from_millis(10));
             }
         }
 
@@ -55,5 +83,16 @@ impl Transmitter for RFM69Transmitter {
         if (*self.serial).flush().is_err() {
             error!("Unable to flush serial port");
         }
+
+        loop {
+            let mut buf = [0 as u8];
+            (*self.serial).read(&mut buf);
+            if buf == eot {
+               break;
+            }
+            thread::sleep(time::Duration::from_millis(10));
+        }
+
+
     }
 }
